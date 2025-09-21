e7b59c49c9dcde7a25b405c48ceb8b1d79526c3e
syukurcode <syukurz87@gmail.com>
2025-09-21 14:08:25 +0800

    Update Docker image tags to use ECR repository for deployment

--- buildnpush.ps1
+++ buildnpush.ps1
@@ -1,7 +1,7 @@
 # Variables
 
 # 1. Build image
-docker build -t "syukurdocker/warrior_backend:latest" .
+docker build -t "794018168409.dkr.ecr.ap-southeast-1.amazonaws.com/syukur/ai_hackathon:latest" .
 
 # 2. Push image
-docker push "syukurdocker/warrior_backend:latest"
+docker push "794018168409.dkr.ecr.ap-southeast-1.amazonaws.com/syukur/ai_hackathon:latest"
